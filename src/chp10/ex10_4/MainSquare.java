116c792b76a94535936939aa659746d0b6a9b6b7
kimkunwoo99 <kimkunwoo99@gmail.com>
2024-10-17 09:54:26 +0900

    업캐스팅: 실습 10 - 4()

---
+++ src/chp10/ex10_4/MainSquare.java
@@ -0,0 +1,10 @@
+package chp10.ex10_4;
+
+public class MainSquare extends Square{
+    public static void main(String[] args) {
+//      Object, Shape, Square 객체의 필드와 메소드 접근가능
+        Square square = new Square();
+//      Shape 객체의 필드와 메소드 접근 가능(예외적으로 Square객체의 멤버를 접근할 수도 있다)
+        Shape shape = new Square();
+    }
+}
